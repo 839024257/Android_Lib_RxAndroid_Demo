b1c78eade8ac19cd18be74e939adb6017e68f6cf
saragitshare <saragitshare@gmail.com>
2016-07-31 22:11:45 +0800

    contains, firt, last demo

--- app/src/main/java/com/hades/android_lib_rxandroid_demo/DemoListClickActivity.java
+++ app/src/main/java/com/hades/android_lib_rxandroid_demo/DemoListClickActivity.java
@@ -2,6 +2,7 @@ package com.hades.android_lib_rxandroid_demo;
 
 import com.hades.android_lib_rxandroid_demo.base.BaseSampleListClickActivity;
 import com.hades.android_lib_rxandroid_demo.base.LvItemBean;
+import com.hades.android_lib_rxandroid_demo.filtering_observables.ContainsFragment;
 import com.hades.android_lib_rxandroid_demo.creating_observables.CreateFragment;
 import com.hades.android_lib_rxandroid_demo.creating_observables.DeferFragment;
 import com.hades.android_lib_rxandroid_demo.creating_observables.FromFragment;
@@ -9,6 +10,8 @@ import com.hades.android_lib_rxandroid_demo.creating_observables.IntervalFragmen
 import com.hades.android_lib_rxandroid_demo.creating_observables.JustFragment;
 import com.hades.android_lib_rxandroid_demo.creating_observables.RepeatFragment;
 import com.hades.android_lib_rxandroid_demo.creating_observables.TimerFragment;
+import com.hades.android_lib_rxandroid_demo.filtering_observables.FilterFragment;
+import com.hades.android_lib_rxandroid_demo.filtering_observables.FirstFragment;
 
 import java.util.List;
 
@@ -55,6 +58,18 @@ public class DemoListClickActivity extends BaseSampleListClickActivity {
                 doPositionClick(new RepeatFragment());
                 break;
 
+            case 8:
+                doPositionClick(new FilterFragment());
+                break;
+
+            case 9:
+                doPositionClick(new FirstFragment());
+                break;
+
+            case 10:
+                doPositionClick(new ContainsFragment());
+                break;
+
             default:
                 break;
         }
@@ -63,6 +78,7 @@ public class DemoListClickActivity extends BaseSampleListClickActivity {
     @Override
     public void createDataSource4Lv(List<LvItemBean> mDataSource) {
         mDataSource.add(0, new LvItemBean("Sample 1", "btn0"));
+
         mDataSource.add(new LvItemBean("Observable.timer()", "btn1"));
         mDataSource.add(new LvItemBean("Observable.interval()", "btn2"));
         mDataSource.add(new LvItemBean("Observable.just()", "btn3"));
@@ -70,6 +86,10 @@ public class DemoListClickActivity extends BaseSampleListClickActivity {
         mDataSource.add(new LvItemBean("Observable.from()", "btn5"));
         mDataSource.add(new LvItemBean("Observable.defer()", "btn6"));
         mDataSource.add(new LvItemBean("Observable.repeat()", "btn7"));
+
+        mDataSource.add(new LvItemBean("filter", "btn8"));
+        mDataSource.add(new LvItemBean("first", "btn9"));
+        mDataSource.add(new LvItemBean("contains", "btn10"));
     }
 
     @Override
---
+++ app/src/main/java/com/hades/android_lib_rxandroid_demo/filtering_observables/FilterFragment.java
@@ -0,0 +1,87 @@
+package com.hades.android_lib_rxandroid_demo.filtering_observables;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.hades.android_lib_rxandroid_demo.R;
+import com.hades.android_lib_rxandroid_demo.base.ISampleFragment;
+
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+import rx.Observable;
+import rx.Subscriber;
+import rx.functions.Func1;
+
+/**
+ * Created by hades on 7/27/2016.
+ */
+public class FilterFragment extends Fragment implements ISampleFragment {
+    public static final String TAG = FilterFragment.class.getSimpleName();
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_filter_layout, container, false);
+
+        ButterKnife.inject(this, view);
+
+        return view;
+    }
+
+    @Override
+    public void onDestroyView() {
+        ButterKnife.reset(this);
+        super.onDestroyView();
+    }
+
+    @Override
+    public String getTAG() {
+        return TAG;
+    }
+
+    @OnClick(R.id.btn1)
+    public void btn1() {
+        Observable.just(1, 10, 20)
+                .filter(new Func1<Integer, Boolean>() {
+                    @Override
+                    public Boolean call(Integer integer) {
+                        return integer > 5;
+                    }
+                }).subscribe(new Subscriber<Integer>() {
+            @Override
+            public void onCompleted() {
+                Log.d(TAG, "onCompleted: ");
+            }
+
+            @Override
+            public void onError(Throwable e) {
+                Log.d(TAG, "onError: ");
+            }
+
+            @Override
+            public void onNext(Integer integer) {
+                Log.d(TAG, "onNext: integer=" + integer);
+            }
+        });
+    }
+//
+//    @OnClick(R.id.btn2)
+//    public void btn2() {
+//
+//    }
+
+    //    @OnClick(R.id.btn3)
+//    public void btn3() {
+//
+//    }
+
+    //    @OnClick(R.id.btn4)
+//    public void btn4() {
+//
+//    }
+}
